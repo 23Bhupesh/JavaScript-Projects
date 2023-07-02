a09ca462894d1059f012232ce4c70ece48bbf279
23Bhupesh <bhupeshsahu2312@gmail.com>
2023-07-03 05:20:47 +0530

    Project 3(Quiz Game)

---
+++ project3/index.js
@@ -0,0 +1,24 @@
+ const questions = [{
+    'que': 'Which of the following is the markup language?',
+    'a': 'HTML',
+    'b': 'CSS',
+    'c': 'javaScript',
+    'd': 'PHP',
+    'correct': 'a'
+ }, {
+    'que': 'What year was javaScript launched?',
+    'a': '1995',
+    'b': '1996',
+    'c': '1994',
+    'd': 'none of these',
+    'correct': 'b'
+ }, {
+    'que': 'What does CSS stands for?',
+    'a': 'Hypertext Markup Language',
+    'b': 'Cascading Style Sheet',
+    'c': 'Jason Object Notation',
+    'd': 'none of these',
+    'correct': 'b'
+ }
+]
+

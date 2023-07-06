c6087c40b82dd5c197e56bfad887e8425445732d
23Bhupesh <bhupeshsahu2312@gmail.com>
2023-07-06 15:55:16 +0530

    Project-3 (Quiz App)

--- project3/index.js
+++ project3/index.js
@@ -20,5 +20,66 @@
     'd': 'none of these',
     'correct': 'b'
  }
-]
+];
 
+let index = 0;
+let total = questions.length;
+let right = 0,
+    wrong = 0;
+const quesBox = document.getElementById("quesBox")
+const optionInputs = document.querySelectorAll(".options")
+const loadQuestion = () => {
+   if(index === total) {
+      return endQuiz()
+   }
+   reset();
+   const data = questions[index]
+   console.log(data)
+   quesBox.innerText = ` ${index+1}) ${data.que}`;
+   optionInputs[0].nextElementSibling.innerText = data.a;
+   optionInputs[1].nextElementSibling.innerText = data.b;
+   optionInputs[2].nextElementSibling.innerText = data.c;
+   optionInputs[3].nextElementSibling.innerText = data.d;
+   
+}
+
+const submitQuiz = () => {
+   const data = questions[index];
+   const ans = getAnswer()
+   if (ans === data.correct) {
+      right++;
+   }else {
+      wrong++;
+   }
+   index++;
+   loadQuestion();
+   return;
+} 
+
+const getAnswer = () => {
+   let answer;
+   optionInputs.forEach(
+      (input) => {
+         if(input.checked) {
+            answer = input.value;
+         }
+      }
+   )
+   return answer;
+}
+
+const reset = () => {
+   optionInputs.forEach(
+      (input) => {
+         input.checked = false;
+      }
+   )
+}
+
+const endQuiz = () => {
+   document.getElementById("box").innerHTML = `
+   <h2>Thank you for playing the Quiz</h2>
+   <h3>${right} / ${total} are correct </h3>
+   `
+}
+loadQuestion ();
